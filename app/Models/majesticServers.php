721e6227748a643bed97e6e4d2c8adce2868511d
GumanXD <56725456+GumanXD@users.noreply.github.com>
2022-08-02 19:29:00 +0300

    1
    1

---
+++ app/Models/majesticServers.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class majesticServers extends Model
+{
+    use HasFactory;
+}
